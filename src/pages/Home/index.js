0a56e5273e4ac80b9b8c38ee3a7ed5e7f869ec54
Sergio Junior <sergio_jfj@hotmail.com>
2020-08-05 12:27:50 -0300

    background p

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -9,6 +9,7 @@ function Home() {
   return (
     <nav>
     <Menu />
+    
     <BannerMain
         videoTitle={dadosIniciais.categorias[0].videos[0].titulo}
         url={dadosIniciais.categorias[0].videos[0].url}
