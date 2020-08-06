12ec6ba9548d30fcff3eabd65f9717311d6b1c46
Sergio Junior <sergio_jfj@hotmail.com>
2020-08-06 09:37:03 -0300

    Cadastro de Categorias

--- src/pages/cadastro/Categoria/index.js
+++ src/pages/cadastro/Categoria/index.js
@@ -1,18 +1,37 @@
-import React from 'react';
+import React, {useState} from 'react';
 import { Link } from 'react-router-dom';
 import PageDefault from '../../../components/PageDefault';
 
 function CadastroCategoria() {
+    const [categorias, setCategorias] = useState(['Teste']) ;
+    
+    const [nomeDaCategoria, setNomeDaCategoria] = useState('Valor Inicial');
+    
+    
   return (
     <PageDefault>
-      <h1>Cadastro de Categoria</h1>
-
-      <form>
+      <h1>Cadastro de Categoria: {nomeDaCategoria}</h1>
+        {/*State*/}
+      <form onSubmit = {function handleSubmit(infosDoEvento){
+        infosDoEvento.preventDefault();
+        console.log('Você tentou enviar o form');
+        setCategorias([
+          ...categorias,
+          nomeDaCategoria
+        ]);
+      }}>
 
         <label>
           Nome da Categoria:
           <input
             type="text"
+            value = {nomeDaCategoria}
+            onChange = {function funcaoHandlerQueOErroPediu(infosDoEvento){
+              /*console.log('[nomeDaCategoria]', nomeDaCategoria);
+              console.log('[infosDoEvento.target.value]', infosDoEvento.target.value);*/
+              setNomeDaCategoria(infosDoEvento.target.value);
+
+          }}
           />
         </label>
 
@@ -20,7 +39,15 @@ function CadastroCategoria() {
           Cadastrar
         </button>
       </form>
-
+        <ul>
+          {categorias.map((categoria, indice) => {
+            return (
+              <li key={ `${categoria} ${indice}` }>
+                {categoria}
+              </li>
+            )
+          })}
+        </ul>
 
       <Link to="/">
         Ir para home
