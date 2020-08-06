d80a9b32a67af0bd5323c3b6f0bcb3b9ab06fd97
Sergio Junior <sergio_jfj@hotmail.com>
2020-08-06 13:07:22 -0300

    ajuste página erro

--- src/index.js
+++ src/index.js
@@ -3,25 +3,42 @@ import ReactDOM from 'react-dom';
 import './index.css';
 
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
-import Home from './pages/Home'
+import Home from './pages/Home';
 import CadastroVideo from './pages/cadastro/Video';
 import CadastroCategoria from './pages/cadastro/Categoria';
+import PageDefault from './components/PageDefault';
+import Erro from './assets/img/erro_404.gif'
 
+// Desafio master blaster na descrição
+// Colocar um jogo ou brincadeira nessa página:
 
-const Pagina404 = () => (<div>Página 404</div>);
+const Pagina404 = () => (
+    <PageDefault>
+  <div style={{ display: 'flex', alignItems: 'center', justifyContent: 'center', flexDirection: 'column' }}>
+    <h1>Ops, quebrou tudo :O</h1>
+    <p>
+      <a href="/">Você pode voltar pra home :)</a>
+    </p>
+    {/*
+      Pessoal, quem quiser fazer o desafio do Flappy Bird, da pra usar esse iframe aqui: 
+      - https://codepen.io/omariosouto/pen/pogmdGE
+      E quem quiser programar o jogo:
+      - https://www.youtube.com/watch?v=jOAU81jdi-c&list=PLTcmLKdIkOWmeNferJ292VYKBXydGeDej
+    */}
+   <div><img src={Erro} alt="Página Quebrada" /></div>
+  </div>
+  </PageDefault>
+)
+
 
 ReactDOM.render(
-    <BrowserRouter>
+  <BrowserRouter>
     <Switch>
-        <Route path="/" component={Home} exact />
-        <Route path="/cadastro/video" component={CadastroVideo} />
-        <Route path="/cadastro/categoria" component={CadastroCategoria} />
-        <Route component={Pagina404} />
+      <Route path="/" component={Home} exact />
+      <Route path="/cadastro/video" component={CadastroVideo} />
+      <Route path="/cadastro/categoria" component={CadastroCategoria} />
+      <Route component={Pagina404} />
     </Switch>
-    </BrowserRouter>,
-//  <React.StrictMode>
-//    <App />
-//  </React.StrictMode>,
+  </BrowserRouter>,
   document.getElementById('root')
 );
-
