132d547b4607f1053364d73d353362712b75f08d
Sergio Junior <sergio_jfj@hotmail.com>
2020-08-11 21:16:42 -0300

    backend aula 04

--- src/pages/cadastro/Categoria/index.js
+++ src/pages/cadastro/Categoria/index.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import PageDefault from '../../../components/PageDefault';
 import FormField from '../../../components/FormField';
@@ -26,6 +26,36 @@ function CadastroCategoria() {
             infosDoEvento.target.value,
         );
     }
+    useEffect(() => {
+        console.log('alo alo w brasil');
+        const URL_TOP = 'http://localhost:8080/categorias';
+        fetch(URL_TOP)
+            .then(async (respostaDoServidor) => {
+                const resposta = await respostaDoServidor.json();
+                setCategorias([
+                    ...resposta,
+                ]);
+            })
+        //setTimeout(() => {
+        //    setCategorias([
+        //        ...categorias,
+        //       {
+        //            "id": 1,
+        //            "nome": "Front End",
+        //            "descricao": "uma categoria bacanudassa",
+        //            "cor": "#6BD1FF"
+        //        },
+        //        {
+        //            "id": 2,
+        //            "nome": "Back End",
+        //            "descricao": "uma categoria bacanudassa",
+        //            "cor": "#6BD1FF"
+        //        }
+
+        //    ]);
+        // }, 4 * 1000);
+
+    }, []);
 
     return (
         <PageDefault>
@@ -69,6 +99,11 @@ function CadastroCategoria() {
                 <Button>
                     Cadastrar
                 </Button>
+
+                {categorias.length === 0 && <div>
+                    {/*Carregando...*/}
+                        Loading...
+                </div>}
             </form>
             <ul>
                 {categorias.map((categoria) => (
