a1c101ee862e02e821e84908caddadb54972783f
Sergio Junior <sergio_jfj@hotmail.com>
2020-08-20 17:46:30 -0300

    Ajustes videos Home

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -8,11 +8,7 @@ import categoriasRepository from '../../repositories/categorias';
 import PageDefault from '../../components/PageDefault'
 
 function Home() {
-
     const [dadosIniciais, setDadosIniciais] = useState([]);
-
-
-
     useEffect(() => {
         categoriasRepository.getAllWithVideos()
             .then((categoriasComVideos) => {
@@ -26,42 +22,34 @@ function Home() {
     }, []);
     //http://localhost:8080/categorias?_embed=videos
     return (
-        <PageDefault>
-            <Menu />
-            {JSON.stringify(dadosIniciais)}
-
-            {/*<BannerMain
-                videoTitle={dadosIniciais.categorias[0].videos[0].titulo}
-                url={dadosIniciais.categorias[0].videos[0].url}
-                videoDescription={"O que é Front-end? Trabalhando na área os termos HTML, CSS e JavaScript fazem parte da rotina das desenvolvedoras e desenvolvedores. Mas o que eles fazem, afinal? Descubra com a Vanessa!"}
-            />
-            <Carousel
-                ignoreFirstVideo
-                category={dadosIniciais.categorias[0]}
-            />
-            <Carousel
-                ignoreFirstVideo
-                category={dadosIniciais.categorias[1]}
-            />
-            <Carousel
-                ignoreFirstVideo
-                category={dadosIniciais.categorias[2]}
-            />
-            <Carousel
-                ignoreFirstVideo
-                category={dadosIniciais.categorias[3]}
-            />
-            <Carousel
-                ignoreFirstVideo
-                category={dadosIniciais.categorias[4]}
-            />
-            <Carousel
-                ignoreFirstVideo
-                category={dadosIniciais.categorias[5]}
-            />*/}
-
+        <PageDefault paddingAll={0}>
+            {dadosIniciais.length === 0 && (<div>Loading..</div>)}
+            {dadosIniciais.map((categoria, indice) => {
+                if (indice === 0) {
+                    return (
+                        <div key={categoria.id}>
+                            <BannerMain
+                                videoTitle={dadosIniciais[0].videos[0].titulo}
+                                url={dadosIniciais[0].videos[0].url}
+                                videoDescription="Ultimo episodio Lançado pelo Viniccius13"
+                            />
+                            <Carousel
+                                ignoreFirstVideo
+                                category={dadosIniciais[0]}
+                            />
+                        </div>
+                    );
+                }
+                return (
+                    <Carousel
+                        key={categoria.id}
+                        category={categoria}
+                    />
+                );
+            })}
         </PageDefault>
     );
 }
+
 
 export default Home;
